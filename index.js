996b2a7e1e87d2c516c00839f315f18df5444a2a
David Ortiz <david.ortiz.p@gmail.com>
2025-04-16 21:14:29 -0500

    updated web server using file system

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// import http from "node:http";
-const http = require("node:http");
+import http from "node:http";
+// const http = require("node:http");
+import fs from "node:fs";
 
 const requestListener = (req, response) => {
   console.log(req.url);
@@ -7,20 +8,74 @@ const requestListener = (req, response) => {
   if (req.url === "/" && req.method === "GET") {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Bienvenido a Notas");
-  } else if (req.url === "/notes") {
+  } else if (req.url === "/notes" && req.method === "GET") {
     response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(JSON.stringify(notes));
-    response.end();
-  } else if (req.url.includes("notes")) {
+
+    fs.readFile("notes.json", (error, content) => {
+      if (error) {
+        console.error(error);
+      }
+
+      const notes = JSON.parse(content);
+      response.write(JSON.stringify(notes));
+      response.end();
+    });
+  } else if (req.url.includes("notes") && req.method === "GET") {
     const id = parseInt(req.url.split("/")[2]);
-    const note = notes.find((n) => n.id === id);
-    if (!note) {
-      response.writeHead(400, { "Content-Type": "application/json" });
-      response.end('{"error": "not found"}');
-    } else {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(note));
-    }
+
+    fs.readFile("notes.json", (error, content) => {
+      if (error) {
+        console.error(error);
+      }
+
+      const notes = JSON.parse(content);
+      const note = notes.find((n) => n.id === id);
+
+      if (!note) {
+        response.writeHead(400, { "Content-Type": "application/json" });
+        response.end('{"error": "not found"}');
+      } else {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(note));
+      }
+    });
+  } else if (req.url === "/notes" && req.method === "POST") {
+    // obtener el cuerpo de la solicitud : objeto JSON
+    console.log("crear nueva nota");
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // convierte el Buffer a string
+    });
+
+    req.on("end", () => {
+      // aquí tienes el cuerpo completo de la solicitud POST
+      body = JSON.parse(body);
+      // const nuevoId = Math.random()*100;
+      // agregar la nota al archivo notes.json
+      fs.readFile("notes.json", (error, content) => {
+        if (error) {
+          console.error(error);
+        }
+
+        const notes = JSON.parse(content);
+
+        const newId = notes.at(-1).id + 1;
+        // const newId = (Math.random() * 100);
+        const note = {
+          id: newId,
+          ...body,
+        };
+        console.log(note);
+
+        notes.push(note);
+
+        // escribir el archivo notes.json con notes que contiene la nueva nota
+        fs.writeFile("notes.json", JSON.stringify(notes), (error) => {
+          if (error) console.error(error);
+        });
+      });
+      response.end();
+    });
   } else {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Solicitud no encontrada");
@@ -29,12 +84,3 @@ const requestListener = (req, response) => {
 
 const server = http.createServer(requestListener);
 server.listen(5500);
-
-const notes = [
-  { id: 1, content: "Hacer la cama" },
-  { id: 2, content: "Lavar los platos" },
-  { id: 3, content: "Estudiar JavaScript" },
-  { id: 4, content: "Hacer las compras" },
-  { id: 5, content: "Limpiar mi habitación" },
-  { id: 6, content: "Almorzar a tiempo" },
-];
